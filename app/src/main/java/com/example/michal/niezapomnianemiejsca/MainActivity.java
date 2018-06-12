4178f3a00c95d5a56e823e8b80a99ecc6c0c9b88
Variott <mborowiec9@gmail.com>
2018-06-12 22:19:15 +0200

    dodane API, wywala się na przejściu do mapy

--- app/src/main/java/com/example/michal/niezapomnianemiejsca/MainActivity.java
+++ app/src/main/java/com/example/michal/niezapomnianemiejsca/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.michal.niezapomnianemiejsca;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+
+        final ArrayList<String> places = new ArrayList<>();
+
+        places.add("Dodaj nowe miejsce....");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,places);
+
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//int i = pozycja, long l = id
+                Toast.makeText(MainActivity.this, i, Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                intent.putExtra("placeNumber", i);
+
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
